7f754f2e09fdb0e151348f65df1179b4121a597f
RemiSaurel <r.saurel5@gmail.com>
2022-08-25 20:39:59 +0200

    setup vuex

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+// store/index.js
+
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    }
+});
